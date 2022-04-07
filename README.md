53ab341b6aa8ceeeda7c7518ae05a6342b157cef
felix-do-wizardry <felix.do.1030@gmail.com>
2022-04-08 02:46:03 +0700

    added base code

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # neoglow
+
+> Arduino IDE
+> Preferences > Additional Boards Manager URLs, add:
+http://arduino.esp8266.com/stable/package_esp8266com_index.json
+
+
+Adafruit ESP8266 (Adafruit)
+Adafruit NeoPixel (Adafruit)
+Websockets (Marcus?)
+ArduinoJson (Benoit Blanchon)
+
+esp8266 (ESP8266 Community)
+
+```bash
+pip install --upgrade -r requirements.txt
+```
--- host_ws.py
+++ host_ws.py
@@ -4,8 +4,6 @@ import json, time, string
 import numpy as np
 import os
 
-
-
 # %%
 def vector_norm(v):
     if not isinstance(v, np.ndarray):
@@ -20,12 +18,16 @@ class PixelSet:
 class PixelSet_Strip(PixelSet):
     def __init__(self,
                 pos_origin=[0, 0, 0],
+                pos_center=None,
                 vector=[1, 1, 1],
                 spacing=16.666667,
                 count=60,
                 ):
         self.vector = vector_norm(vector)
-        self.pos_origin = np.array(pos_origin)
+        if pos_center is not None:
+            self.pos_origin = np.array(pos_center) - self.vector * spacing * (count - 1) / 2
+        else:
+            self.pos_origin = np.array(pos_origin)
         pos = self.pos_origin[None] + self.vector[None] * spacing * np.arange(count)[:, None]
         super().__init__(pos=pos)
 
@@ -93,23 +95,23 @@ class PixelSet_Matrix_Zigzag(PixelSet):
 
 
 # %%
-ps_stand = PixelSet_Frame(
-    pos_origin=[1200, 100, 900],
-    vectors=[[1, 0, 0], [0, 1.8, 1]],
-    spacing=1000/60,
-    size=[12, 8],
-    corner_offset=1.,
-)
-ps_stand.pos.shape
+# ps_stand = PixelSet_Frame(
+#     pos_origin=[1200, 100, 900],
+#     vectors=[[1, 0, 0], [0, 1.8, 1]],
+#     spacing=1000/60,
+#     size=[12, 8],
+#     corner_offset=1.,
+# )
+# ps_stand.pos.shape
 
 # %%
-ps_m32x8 = PixelSet_Matrix_Zigzag(
-    pos_origin=[1500, 400, 750],
-    vectors=[[0, -1, 0], [1, 0, 0]],
-    spacing=1000/100,
-    size=[32, 8],
-)
-ps_m32x8.pos.shape
+# ps_m32x8 = PixelSet_Matrix_Zigzag(
+#     pos_origin=[1500, 400, 750],
+#     vectors=[[0, -1, 0], [1, 0, 0]],
+#     spacing=1000/100,
+#     size=[32, 8],
+# )
+# ps_m32x8.pos.shape
 
 # %%
 def float2hex(v=1.0, digit=2, fill=True):
@@ -226,37 +228,41 @@ class NeoControl:
             )
 
 # %%
-tv_mid_x = 640
+tv_mid_x = 700
+tv_y = 120
 desk_z = 750
 ps_edge = PixelSet_Strip(
-    pos_origin=[600 + 29.5 * 1000/60, 0, desk_z],
+    pos_center=[600, 0, desk_z],
     vector=[-1, 0, 0],
     spacing=1000/60,
     count=60,
 )
-ps_edge.pos.shape
 ps_tv_bottom = PixelSet_Strip(
-    pos_origin=[tv_mid_x + 17.5 * 1000/60, 180, desk_z + 80],
+    pos_center=[tv_mid_x, tv_y, desk_z + 80],
     vector=[-1, 0, 0],
-    spacing=1000/60,
-    count=36,
+    spacing=1000/144,
+    count=144,
 )
 ps_tv_top = PixelSet_Strip(
-    pos_origin=[tv_mid_x - 31.5 * 1000/60, 100, desk_z + 700],
+    pos_center=[tv_mid_x, tv_y, desk_z + 700],
     vector=[1, 0, 0],
     spacing=1000/60,
-    count=64,
+    count=60,
 )
 ps_edge_tv = PixelSet(
-    pos=np.concatenate([ps_edge.pos, ps_tv_bottom.pos, ps_tv_top.pos], axis=0),
+    pos=np.concatenate([
+        ps_edge.pos,
+        ps_tv_bottom.pos,
+        ps_tv_top.pos,
+    ], axis=0),
 )
 ps_edge_tv.pos.shape
 
-
+# %%
 NC = NeoControl(
     ip_prefix='192.168.3',
-    # ip_values=[101],
-    ip_values=[103],
+    ip_values=[101],
+    # ip_values=[103],
     pixel_pos=[ps_edge_tv.pos],
     # pixel_pos=[ps_edge.pos],
     # pixel_pos=[ps_m32x8.pos],
@@ -264,31 +270,112 @@ NC = NeoControl(
 )
 NC
 
-def get_cycle(vector=[1, 0, 0]):
+# %%
+def get_cycle(vector=[1, 0, 0], cycle_length=600, time_duration=1000):
     def cycle(pos, **kwargs):
-        _velocity = kwargs.get('velocity', 30)
-        _time_duration = kwargs.get('time_duration', 2000)
-        return ((pos @ vector_norm([vector]).reshape(-1, 1) / _velocity) * 1000)
+        # _velocity = kwargs.get('velocity', 30)
+        # _time_duration = kwargs.get('time_duration', 2000)
+        return ((pos @ vector_norm([vector]).reshape(-1, 1) / cycle_length) * time_duration)
         # return ((pos @ vector_norm([[1, 0.6, 0]]).reshape(-1, 1) / _velocity) * 1000) % _time_duration
     
     return cycle
 
-_time_duration = 20_000
+_velocity = 120
+_cycle_length = 1600
+_time_duration = _cycle_length / _velocity * 1000
+# _time_duration = 20_000
+
 NC.send(
     # get_cycle([1, 0, 0]),
-    get_cycle([1, 1, -0.7]),
+    get_cycle(
+        # vector=[1, 1, -0.7],
+        vector=[1, 0, 0],
+        cycle_length=_cycle_length,
+        time_duration=_time_duration,
+    ),
     fn_kwargs=dict(
         # velocity=2000,
         # velocity=100,
-        velocity=1_000_000 / _time_duration * 2
+        # velocity=1_000_000 / _time_duration * 2,
     ),
     # time_duration=12000,
     time_duration=_time_duration,
     hue_base=0.,
     hue_delta=1.,
     saturation=1.,
-    value=0.25,
+    # value=0.25,
+    value=0.5,
 )
+
+# %%
+import plotly.express as px
+import plotly.graph_objects as go
+
+# %%
+_pos = ps_edge_tv.pos
+fig = go.Figure(data=[go.Scatter3d(
+    x=_pos[:, 0],
+    y=_pos[:, 1],
+    z=_pos[:, 2],
+    mode='markers',
+    marker=dict(
+        size=8,
+        color=np.arange(_pos.shape[0]),
+        # colorscale='Viridis',   # choose a colorscale
+        opacity=1
+    )
+)])
+
+fig.add_trace(
+    go.Mesh3d(
+        x=[0, 0, 1200, 1200],
+        y=[0, 600, 0, 600],
+        z=[750] * 4,
+        # colorbar_title='z',
+        colorscale=[[0, 'rgb(200, 200, 200)'], [1, 'rgb(200, 200, 200)']],
+        # colorscale=[[0, 'gold'],
+        #             [0.5, 'mediumturquoise'],
+        #             [1, 'magenta']],
+        # Intensity of each vertex, which will be interpolated and color-coded
+        intensity=[0] * 4,
+        # i, j and k give the vertices of triangles
+        # here we represent the 4 triangles of the tetrahedron surface
+        # i=[0, 0, 0, 1],
+        # j=[1, 2, 3, 2],
+        # k=[2, 3, 1, 3],
+        name='desk_1',
+        showscale=False,
+    )
+)
+
+fig.add_trace(
+    go.Mesh3d(
+        x=[v + 1220 for v in [0, 0, 1200, 1200]],
+        y=[0, 600, 0, 600],
+        z=[750] * 4,
+        colorscale=[[0, 'rgb(50, 50, 50)'], [1, 'rgb(50, 50, 50)']],
+        intensity=[0] * 4,
+        name='desk_0',
+        showscale=False,
+    )
+)
+
+_max_value = max(np.max(_pos) * 1.1, 2500)
+fig.update_layout(
+    scene = dict(
+        xaxis = dict(nticks=4, range=[0, _max_value]),
+        yaxis = dict(nticks=4, range=[0, _max_value]),
+        zaxis = dict(nticks=4, range=[0, _max_value]),
+        aspectmode='cube',
+    ),
+    # yaxis=dict(scaleanchor="x", scaleratio=1),
+    # zaxis=dict(scaleanchor="x", scaleratio=1),
+    template='plotly_dark',
+    margin=dict(l=0, r=0, b=0, t=0),
+    width=800,
+    height=800,
+)
+fig.show()
 
 # %%
 def ping(ip, delay=0.2):
@@ -316,7 +403,7 @@ print()
 neos = {
     v: NeoWS(ip=f'192.168.3.{v + 100}', port=8908)
     # for v in [101, 103][:]
-    for v in [1]
+    for v in [3]
 }
 
 # %%
